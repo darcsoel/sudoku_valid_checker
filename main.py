114098e4fe24691f07cfb05c8f61ef5ed8a8bfb2
Ross <rostikvunarchuk@gmail.com>
2022-02-03 00:09:27 +0200

    solution

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Solution:
+    BOARD_LENGTH = 9
+    valid_symbols = list(map(str, (range(10)))) + ['.']
 
+    def __init__(self, board: List[List[str]]):
+        self._board = board
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def _check_sub_boxes(self):
+        for col_index in range(self.BOARD_LENGTH):
+            for row_index in range(self.BOARD_LENGTH):
+                if col_index % 3 == row_index % 3 == 0:
+                    line = []
+                    for inner_col_index in range(col_index, col_index + 3):
+                        for inner_row_index in range(row_index, row_index + 3):
+                            line.append(self._board[inner_col_index][inner_row_index])
+                    if not self._check_line(line):
+                        return False
+        return True
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def _check_line(self, line):
+        symbols = [x for x in line if x in self.valid_symbols]
+
+        if symbols != line:
+            return False
+
+        symbols = [x for x in symbols if x.isdigit()]
+
+        if len(symbols) != len(set(symbols)):
+            return False
+
+        return True
+
+    def _check_columns(self):
+        for index in range(self.BOARD_LENGTH):
+            line = [self._board[column][index] for column in range(self.BOARD_LENGTH)]
+            if not self._check_line(line):
+                return False
+
+        return True
+
+    def _check_rows(self):
+        for row in self._board:
+            if not self._check_line(row):
+                return False
+
+        return True
+
+    def is_valid(self) -> bool:
+        sub_boxes = self._check_sub_boxes()
+        columns = self._check_columns()
+        rows = self._check_rows()
+
+        return all((sub_boxes, columns, rows))
