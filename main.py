9185cfeee790e225409db503ceca5ec4f43b2f75
Ross <rostikvunarchuk@gmail.com>
2022-02-05 18:33:44 +0200

    refactoring

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from typing import List
 
 class Solution:
     BOARD_LENGTH = 9
+    BOX_SIZE = 3
     valid_symbols = list(map(str, (range(10)))) + ['.']
 
     def __init__(self, board: List[List[str]]):
         self._board = board
 
     def _check_sub_boxes(self):
-        for col_index in range(self.BOARD_LENGTH):
-            for row_index in range(self.BOARD_LENGTH):
-                if col_index % 3 == row_index % 3 == 0:
-                    line = []
-                    for inner_col_index in range(col_index, col_index + 3):
-                        for inner_row_index in range(row_index, row_index + 3):
-                            line.append(self._board[inner_col_index][inner_row_index])
-                    if not self._check_line(line):
-                        return False
+        for col_index in range(0, self.BOARD_LENGTH, self.BOX_SIZE):
+            for row_index in range(0, self.BOARD_LENGTH, self.BOX_SIZE):
+                line = []
+                for inner_col_index in range(col_index, col_index + self.BOX_SIZE):
+                    for inner_row_index in range(row_index, row_index + self.BOX_SIZE):
+                        line.append(self._board[inner_col_index][inner_row_index])
+                if not self._check_line(line):
+                    return False
         return True
 
     def _check_line(self, line):
